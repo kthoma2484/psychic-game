9bfb7f9a26895e838009289dfaa572e757ebd564
KKT84 <35786891+kthoma2484@users.noreply.github.com>
2018-05-27 15:29:42 -0700

    added some additional actions to finished game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,16 +5,17 @@ $(document).ready(function(){
     let numLoss = 1;
     let numLeft= 9;
 
-
+    let winningLetter = "";
+    let playerLetter = "";
+    let celebrateImage = 'http://bestanimations.com/Holidays/Fireworks/fireworks/fireworks-animated-gif-32.gif';
+    
     // computer picks random letter
     
     let theLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
     console.log("All letters in array available")
 
-    let winningLetter = "";
-    let playerLetter = "";
- 
-
+  
+    // computer selects random letter on page load
     let newWinningLetter = function () {
         winningLetter = theLetters[Math.floor(Math.random() * theLetters.length)];
         console.log("this is the winning letter = " + winningLetter);
@@ -22,9 +23,19 @@ $(document).ready(function(){
     
     newWinningLetter();
 
+
     $(document).keypress(function(event) {
-       
         
+        // clears the element that displays the computer letter for game reset
+        if ( numLeft == 9) {
+            $('#computerLetterValue').empty();
+            $('img').attr('src', "");
+        }  
+
+        // clears the winner celebration image for game reset
+     
+
+        // creates the concat string of player letter guesses
         playerLetter = String.fromCharCode(event.which);
         console.log("this is the player guess = " + playerLetter);
 
@@ -39,10 +50,10 @@ $(document).ready(function(){
             } else if ( winningLetter == playerLetter && playerLetter == theLetters[i]) {
                 // if player guess is same as winning number, then...
                 $("#winValue").html(numWins++); // win value increases by 1
-                $("#computerLetterValue").html(winningLetter); // winning letter displayed
                 $("#guessLeftValue").html(numLeft = 9); // guesses left resets 
                 $("#guessMadeValue").empty(); // letters guessed resets
                 alert("Yes! You got it right. The winning letter is " + "'" + winningLetter + "'" + ". You get a high-five!"); // winner alert displays
+                $('img').attr('src', celebrateImage);
                 newWinningLetter();
             }
         
@@ -51,11 +62,11 @@ $(document).ready(function(){
                 $("#guessLeftValue").html(numLeft = 9); // guesses left resets
                 $("#guessMadeValue").empty(); // letters guessed resets
                 $("#lossValue").html(numLoss++); // losses value increases by 1
-                $("#computerLetterValue").html("- My letter was " + "'" + winningLetter + "'");
-                newWinningLetter();
-                
+                $("#computerLetterValue").html("My letter was " + "'" + winningLetter + "'. Another one bites the dust!");
+                newWinningLetter(); 
             }
         }
+        
     });
    
 });
